936d0fc0a4b8603f801dd07f2cfbf1a5d71590ca
Oliver Dew <oliver.dew@kinandcarta.com>
2023-08-01 22:25:49 +0100

    Initial Commit

---
+++ MorphTargetExample/MorphTargetExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MorphTargetExampleApp.swift
+//  MorphTargetExample
+//
+//  Created by Oliver Dew on 01/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct MorphTargetExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
