65f58f6b5ba3f7e6a134ac2bc44163e778966427
Oliver Dew <oliver.dew@kinandcarta.com>
2023-08-14 16:42:57 +0100

    SPM-friendly folder structure

--- Package.swift
+++ Package.swift
@@ -7,14 +7,11 @@ let package = Package(
     name: "RealityMorpher",
 	platforms: [.iOS(.v15), .macOS(.v12)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "RealityMorpher",
             targets: ["RealityMorpher"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
 			name: "RealityMorpher", dependencies: ["RealityMorpherKernels"]
 		),
